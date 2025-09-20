617d984c6eba7eda77b86cf6062850f9659b4a90
RenewedEarl <renewedearl2025@gmail.com>
2025-09-20 15:50:27 -0400

    Fleshed out Evergreen Town
    I added more trigger events as well as added a
    bit more flora as well as setup the potential for
    more events to take place here.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -46,10 +46,10 @@
 #define FLAG_RECEIVED_EVERGREEN_POTION    0x20
 #define FLAG_START_WARP_FINISHED          0x21
 #define FLAG_MOM_CAME_UPSTAIRS            0x22
-#define FLAG_STARTER_BALL_ONE             0x23
-#define FLAG_STARTER_BALL_TWO             0x24
-#define FLAG_STARTER_BALL_THREE           0x25
-#define FLAG_UNUSED_0x026    0x26 // Unused Flag
+#define FLAG_RIVAL_VISIBILITY             0x23
+#define FLAG_STARTER_BALL_ONE             0x24
+#define FLAG_STARTER_BALL_TWO             0x25
+#define FLAG_STARTER_BALL_THREE           0x26
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
 #define FLAG_UNUSED_0x029    0x29 // Unused Flag
